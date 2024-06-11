cd51697a6ea72e8b78ab97de07043c477d7f49e4
Evelina Agostini <evelina.agostini@elbuild.it>
2024-06-11 11:08:30 +0200

    update angular 17, rename, fix

--- libs/ngx-cleave-zen/jest.config.js
+++ libs/ngx-cleave-zen/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'ngx-cleave',
+  name: 'ngx-cleave-zen',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/ngx-cleave',
+  coverageDirectory: '../../coverage/libs/ngx-cleave-zen',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
